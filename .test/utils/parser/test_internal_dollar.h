225aec8474f8dbde9c6be7d1127ecef9e324b481
hsoysal <mathieu.soysal@outlook.fr>
2024-07-18 05:10:00 +0200

    add valid tests

--- .test/utils/parser/test_internal_dollar.h
+++ .test/utils/parser/test_internal_dollar.h
@@ -6,7 +6,7 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 20:20:18 by hsoysal           #+#    #+#             */
-/*   Updated: 2024/07/18 05:04:42 by hsoysal          ###   ########.fr       */
+/*   Updated: 2024/07/18 05:09:45 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,12 +78,44 @@ void	test_apply_dollars4(void)
 	free(actual);
 }
 
+void	test_apply_dollars5(void)
+{
+	char	*command_line;
+	char	*expected;
+	char	*actual;
+	char	*env[] = {"USER=hsoysal", NULL};
+
+	command_line = ft_strdup("\"$\"");
+	expected = ft_strdup("\"$\"");
+	actual = apply_dollars(command_line, env);
+	assert_equals(expected, actual);
+	free(expected);
+	free(actual);
+}
+
+void	test_apply_dollars6(void)
+{
+	char	*command_line;
+	char	*expected;
+	char	*actual;
+	char	*env[] = {"USER=hsoysal", NULL};
+
+	command_line = ft_strdup("\"$s\"");
+	expected = ft_strdup("\"\"");
+	actual = apply_dollars(command_line, env);
+	assert_equals(expected, actual);
+	free(expected);
+	free(actual);
+}
+
 void	test_apply_dollars_all(void)
 {
 	test_apply_dollars1();
 	test_apply_dollars2();
 	test_apply_dollars3();
 	test_apply_dollars4();
+	test_apply_dollars5();
+	test_apply_dollars6();
 }
 
 #endif // TEST_INTERNAL_DOLLAR_H
