7d4d3877f1f6754a92cee219920d77cd0bd0fb56
hsoysal <mathieu.soysal@outlook.fr>
2024-07-31 17:06:41 +0200

    chore: Update mini_libft.h to include libft.h

--- src/utils/mini_libft/mini_libft.h
+++ src/utils/mini_libft/mini_libft.h
@@ -6,13 +6,14 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 14:45:19 by hsoysal           #+#    #+#             */
-/*   Updated: 2024/07/31 11:20:25 by hsoysal          ###   ########.fr       */
+/*   Updated: 2024/07/31 17:06:14 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINI_LIBFT_H
 # define MINI_LIBFT_H
 
+# include "../../libft/libft.h"
 # include "../../structures/double_linked_list/double_linked_list.h"
 # include <aio.h>
 # include <stdbool.h>
@@ -22,18 +23,11 @@
 # endif
 
 char	*ft_strndup(const char *s, size_t n);
-char	*ft_strdup(const char *s);
-char	*ft_strchr(const char *s, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_strcmp(const char *s1, const char *s2);
 bool	equals(const char *s1, const char *s2);
-int		ft_strlen(const char *s);
 char	*concatenate(t_double_linked_list *list);
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_word_until(char const *command_line, size_t *i,
 			char const delimiter);
 char	*get_from_env(char **env, char *word);
 char	*ft_get_next_line(int fd);
-void	ft_putstr_fd(char *s, int fd);
 
 #endif // MINI_LIBFT_H
