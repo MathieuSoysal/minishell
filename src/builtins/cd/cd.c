431ec4072dd8c93ca40e0f1791eb9dc5e256720b
hsoysal <mathieu.soysal@outlook.fr>
2024-08-10 07:45:32 +0200

    fix builtin cd

--- src/builtins/cd/cd.c
+++ src/builtins/cd/cd.c
@@ -6,10 +6,11 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 19:00:54 by kahoumou          #+#    #+#             */
-/*   Updated: 2024/08/08 10:53:51 by hsoysal          ###   ########.fr       */
+/*   Updated: 2024/08/10 07:44:24 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../libft/libft.h"
 #include "../../structures/commande/commande.h"
 #include "../utils/utils.h"
 #include <errno.h>
@@ -28,8 +29,10 @@ int	cd(t_commande *cmd, char **envp)
 		return (0);
 	if (chdir(cmd->args[1]) == -1)
 	{
-		perror("No such file or directory\n");
-		return (1);
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(cmd->args[1], 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		return (2);
 	}
 	env_update_var(envp, "PWD", getcwd(NULL, 0));
 	return (0);
