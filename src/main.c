7f157b8c7b5cf3b1cf6bc0a0eaa3b5a3040fde77
hsoysal <mathieu.soysal@outlook.fr>
2024-08-16 09:01:33 +0200

    refactor main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:46:57 by hsoysal           #+#    #+#             */
-/*   Updated: 2024/08/16 01:48:03 by hsoysal          ###   ########.fr       */
+/*   Updated: 2024/08/16 08:58:35 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,52 @@ static void	execute_alll_commands(t_commande **commands, char ***g_env)
 		execute_single_command(commands[i], g_env);
 }
 
+static bool	is_not_empty(const char *str)
+{
+	if (str && *str && str[0] != '\n' && str[0] != '\0')
+	{
+		while (*str)
+		{
+			if (*str != ' ' && *str != '\t')
+				return (true);
+			str++;
+		}
+	}
+	return (false);
+}
+
+static bool	is_a_valid_command_line(const char *command_line)
+{
+	t_syntax_error	error;
+
+	error = check_syntax(command_line);
+	if (error != NO_ERROR)
+		return (print_error(error), false);
+	return (is_not_empty(command_line));
+}
+
+static char	*read_command_line(void)
+{
+	char	*command_line;
+	char	*prompt;
+
+	prompt = get_prompt();
+	command_line = readline(prompt);
+	free(prompt);
+	if (command_line == NULL)
+	{
+		free_env(*get_envp(NULL));
+		write(1, "exit\n", 6);
+		exit(get_exit_status(_LAST_STATUS));
+	}
+	return (command_line);
+}
+
 int	main(int argc, char const *argv[], char *envp[])
 {
-	char			*command_line;
-	t_syntax_error	error;
-	t_commande		**commands;
-	char			***g_env;
-	char			*prompt;
+	char		*command_line;
+	t_commande	**commands;
+	char		***g_env;
 
 	(void)argc;
 	(void)argv;
@@ -47,16 +86,8 @@ int	main(int argc, char const *argv[], char *envp[])
 	g_env = get_envp(envp);
 	while (1)
 	{
-		prompt = get_prompt();
-		command_line = readline(prompt);
-		free(prompt);
-		if (command_line == NULL)
-			return (free_env(*g_env), write(1, "exit\n", 6),
-				get_exit_status(_LAST_STATUS));
-		error = check_syntax(command_line);
-		if (error != NO_ERROR)
-			print_error(error);
-		else if (command_line[0] != '\n' && command_line[0] != '\0')
+		command_line = read_command_line();
+		if (is_a_valid_command_line(command_line))
 		{
 			add_history(command_line);
 			commands = parse_command_line(command_line, *g_env);
