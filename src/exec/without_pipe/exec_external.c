1a631df3f10f555e476874fd22ea6b5d3f259561
Kamel Houmouad <kahoumou@student.42w>
2024-11-26 14:30:04 +0100

    resolve leak

--- src/exec/without_pipe/exec_external.c
+++ src/exec/without_pipe/exec_external.c
@@ -6,7 +6,7 @@
 /*   By: kahoumou <kahoumou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 03:09:48 by hsoysal           #+#    #+#             */
-/*   Updated: 2024/11/25 22:18:34 by kahoumou         ###   ########.fr       */
+/*   Updated: 2024/11/26 14:27:21 by kahoumou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,13 @@ void handle_output_redirection(t_commande *command, t_commande **commands, char 
 
 void handle_execve_failure(t_commande **commands, t_commande *command, char ***g_env)
 {
+    (void)g_env;
     ft_putstr_fd("minishell: ", 2);
     ft_putstr_fd(command->args[0], 2);
     ft_putstr_fd(": ", 2);
     ft_putstr_fd(strerror(errno), 2);
     ft_putstr_fd("\n", 2);
     free_commands(commands);
-    free_env(*g_env);
     if (errno == EACCES)
         exit(126);
     else if (errno == ENOENT)
@@ -84,6 +84,7 @@ void execute_external_command_bis(t_commande **commands, t_commande *command, ch
         handle_execve_failure(commands, command, g_env);
 
     free_commands(commands);
+    free_env(*g_env);
     exit(status);
 }
 
